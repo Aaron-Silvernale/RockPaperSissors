a3efd6d25def6388a4ac8fecdd4412364c09df0a
Aaron-Silvernale <135292960+Aaron-Silvernale@users.noreply.github.com>
2024-08-29 16:07:28 -0600

    first draft layout, troubleshooting comparison issues

--- javascript.js
+++ javascript.js
@@ -10,17 +10,46 @@ function getComputerChoice() {
 
 }
 
-let computer = getComputerChoice();
-console.log(computer);
+let computerChoice = getComputerChoice();
+console.log(computerChoice)
 
 function getHumanChoice() {
     let a = prompt("Rock, Paper, Scissors?", );
     return a.toLowerCase();
 }
 
-let player =  getHumanChoice();
-console.log(player);
-
+let humanChoice =  getHumanChoice();
+console.log(humanChoice)
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == rock && computerChoice == scissors) {
+        humanScore + 1;
+        return "You win! Rock beats Scissors!";
+    } else if (humanChoice == "rock" && computerChoice == "rock") {
+        return "Draw!";
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        computerScore + 1;
+        return "You lose! Paper beats Rock";
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore + 1;
+        return ("You win!" + humanChoice + " beats " + computerChoice);
+    } else if (humanChoice == "scissors" && computerChoice == "scissors") {
+        return "Draw!";
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore + 1;
+        return ("You lose!" + computerChoice + " beats " + humanChoice);
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore + 1;
+        return ("You win!" + humanChoice + " beats " + computerChoice);
+    } else if (humanChoice == "paper" && computerChoice == "paper") {
+        return "Draw!";
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        computerScore + 1;
+        return ("You lose!" + computerChoice + " beats " + humanChoice);
+    } else {
+        return "WTF was that?";
+    }
+}
+console.log(playRound())
